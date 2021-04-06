b52e79730e04a7200491903955754d9117b649af
ethanario <ethanspam13@gmail.com>
2021-04-05 23:15:58 -0500

    changes to TypingPractice

---
+++ src/TypingPractice/TypingPractice.java
@@ -0,0 +1,138 @@
+package TypingPractice;
+
+import java.util.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+
+
+public class TypingPractice {
+
+    public String[] menu;
+    public String username;
+
+
+    public static void main(String[] args) {
+        TypingPractice typingPractice = new TypingPractice();
+        typingPractice.run();
+    }
+
+    public TypingPractice(){
+        this.menu = getMenu();
+        this.username = getUser();
+    }
+
+    //Add new menu items here
+    public String[] getMenu(){
+        String[] mainMenu = {"1 Minute", "3 Minutes", "Custom", "View Previous Scores", "Quit"};
+        return mainMenu;
+    }
+
+    public String getUser(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your current or desired username");
+        String s = in.nextLine();
+        return s;
+    }
+
+
+
+    // Creates the menu that allows the user to select their game mode.
+    public void display(){
+        System.out.println();
+        System.out.println("----------------------------------------------");
+        System.out.println("                     MENU                     ");
+        System.out.println("----------------------------------------------");
+        for (int i = 0; i < this.menu.length; i++)
+        {
+            System.out.println();
+            System.out.println(this.menu[i]);
+            System.out.println();
+            if (i != this.menu.length-1){
+                System.out.println("- - - - - - - - - - - - - - - - - - - - - - -");
+            }
+        }
+        System.out.println("----------------------------------------------");
+        System.out.println();
+
+    }
+
+    public void run(){
+        boolean quit = false;
+        while (!quit) {
+            display();
+            Scanner in = new Scanner(System.in);
+            System.out.println("What would you like to do?");
+            String input = in.nextLine();
+            if (input.equalsIgnoreCase("quit")) {
+                quit = true;
+                return;
+            } else if (input.equalsIgnoreCase("Custom")) {
+                GameMode game = new TimedPractice(this.username, getGameLength());
+                game.run();
+            }
+            else if (input.equalsIgnoreCase("1 Minute")) {
+                GameMode game = new TimedPractice(this.username, 60);
+                game.run();
+            }
+            else if (input.equalsIgnoreCase("3 Minute")) {
+                GameMode game = new TimedPractice(this.username, 180);
+                game.run();
+            }
+            else if (input.equalsIgnoreCase("View Previous Scores")) {
+                retrieveGame();
+            }
+        }
+        System.out.println("Thanks for playing.");
+    }
+
+    public void retrieveGame(){
+
+        File dir = new File("src/resources/game_reports/");
+        File[] files = dir.listFiles((dir1, name) -> name.startsWith(username+"-"));
+        for (int i = 0; i < files.length; i++){
+            System.out.println(i+1 + ".    " + files[i].getName());
+        }
+        Scanner in = new Scanner(System.in);
+        System.out.println("Which game report would you like to view?");
+        int s = in.nextInt();
+        if (s > 0 || s <= files.length){
+            File gameFile = files[s-1];
+
+            TimedPractice e = null;
+            try {
+                FileInputStream fileIn = new FileInputStream(gameFile.toString());
+                ObjectInputStream inStream = new ObjectInputStream(fileIn);
+                e = (TimedPractice) inStream.readObject();
+                inStream.close();
+                fileIn.close();
+            } catch (IOException i) {
+                i.printStackTrace();
+                return;
+            } catch (ClassNotFoundException c) {
+                System.out.println("Game not found");
+                c.printStackTrace();
+                return;
+            }
+
+            System.out.println();
+            System.out.println("Game Report:");
+            System.out.println("Name: " + e.username);
+            System.out.println("Game Length: " + e.gameLength);
+            System.out.println("Number of correct words: " + e.correctWords.length);
+            System.out.println("Number of incorrect words: " + e.incorrectWords.length);
+        }
+    }
+
+    public float getGameLength(){
+        float s = 0;
+        while (s <= 0 || s > 120){
+            Scanner in = new Scanner(System.in);
+            System.out.println("How many seconds would you like this round to last? Enter a number between 0-120");
+            s = in.nextFloat();
+        }
+        return s;
+    }
+
+}
---
+++ src/tests/setSystemOut.java
@@ -0,0 +1,27 @@
+package tests;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.rules.ExternalResource;
+
+public class setSystemOut extends ExternalResource {
+
+    private PrintStream sysOut;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Override
+    protected void before() throws Throwable {
+        sysOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @Override
+    protected void after() {
+        System.setOut(sysOut);
+    }
+
+    public String asString() {
+        return outContent.toString();
+    }
+}

c069297f78189d1f0635e1deccf35c0587158771
ethanario <ethanspam13@gmail.com>
2021-04-12 21:38:53 -0500

    added comments for existing classes

--- src/TypingPractice/QuitItem.java
+++ src/TypingPractice/QuitItem.java
@@ -1,5 +1,9 @@
 package TypingPractice;
 
+/*
+ * Inherits from game mode and is called at the main menu to end a session
+ */
+
 public class QuitItem extends GameMode {
     public void run(){
         System.exit(0);

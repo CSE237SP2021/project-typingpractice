1422079164efb92773609926373ab630baa7c570
ethanario <50820992+ethanario@users.noreply.github.com>
2021-04-27 22:32:39 -0500

    Create TypingPracticeTest.java

---
+++ src/tests/TypingPracticeTest.java
@@ -0,0 +1,71 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import TypingPractice.TypingPractice;
+
+class TypingPracticeTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private String username = "e";
+    private TypingPractice typingPractice;
+   
+    
+    @BeforeEach 
+    void setOutput() {
+        System.setOut(new PrintStream(outContent));
+        System.setIn(new ByteArrayInputStream(username.getBytes()));
+    	typingPractice = new TypingPractice();
+    }
+    
+    
+	@Test
+    void testUsernamePrompt(){
+        assertEquals(outContent.toString(), "Please enter your current or desired username\n");
+    }
+	
+	
+	@Test
+    void testMenuLength(){
+        assertEquals(typingPractice.menu.length, 7);
+    }
+    
+    @Test
+    void testTypingPracticeUsername(){    	
+        assertEquals(typingPractice.username, username);
+    }
+    
+    
+    @Test
+    void testQuit () {    	
+        assertEquals(typingPractice.getMenu()[6], "Quit");
+    }
+    
+    @Test
+    void testLogOutNewUser() { 
+    	String newUsername = "b";
+    	System.setIn(new ByteArrayInputStream(newUsername.getBytes()));
+        typingPractice.logOut();
+        assertEquals(typingPractice.username, newUsername);
+
+    }
+    
+    
+    @Test
+    void testLogOutQuit() { 
+    	String newUsername = "quit";
+    	System.setIn(new ByteArrayInputStream(newUsername.getBytes()));
+        typingPractice.logOut();
+        assertEquals(outContent.toString().split("\n")[2], "Thanks for playing.");
+    }
+    
+
+
+}
